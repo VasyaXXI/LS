16013adeac0d920e866aadd0b08af174dd14c591
VasyaXXI <31704859+VasyaXXI@users.noreply.github.com>
2017-12-04 17:13:58 +0300

    Add files via upload

---
+++ hjhjhjh.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main (int argc, char *argv[])
+{
+	DIR *dir;
+	struct dirent * sd;
+	char mfile[127];
+
+	if (argc < 2)
+	{
+		strcpy(mfile, ".");
+	}
+	else
+        strcpy(mfile, argv[1]);
+
+        printf("%s \n \n", mfile);
+        dir = opendir(mfile);
+
+	while((sd = readdir(dir)) != NULL)
+	{
+		printf("[%s] \n", sd->d_name);
+		sd = readdir(dir);
+	}
+
+	closedir(dir);
+
+	return 0;
+}
