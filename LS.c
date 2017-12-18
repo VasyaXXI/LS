1ded1937b6eb434ae5d2839f1175507fc5864619
VasyaXXI <31704859+VasyaXXI@users.noreply.github.com>
2017-12-18 18:18:48 +0300

    Update LS.c

--- LS.c
+++ LS.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <string.h>
-#include <sys/types.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "dirent.h"
+#include "string.h"
+#include "sys/types.h"
 
 int main (int argc, char *argv[])
 {
